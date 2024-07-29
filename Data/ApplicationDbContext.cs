7fc8db2439ae335524da9400806396315055bef4
levandu12 <146940172+levandu12@users.noreply.github.com>
2024-07-29 13:07:20 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp3.Models;
+
+namespace BlazorApp3.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorApp3.Models.Semesters> Semesters { get; set; } = default!;
+        public DbSet<BlazorApp3.Models.Departments> Departments { get; set; } = default!;
+        public DbSet<BlazorApp3.Models.Majors> Majors { get; set; } = default!;
+        public DbSet<BlazorApp3.Models.Subjects> Subjects { get; set; } = default!;
+       
+    
+      
+    }
+}
